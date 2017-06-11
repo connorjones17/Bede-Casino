a0a26c87835f5c0dcc8ae3b7fb3b340bdb4c760c
connorjones17 <connor.jones@mayden.co.uk>
2017-06-11 22:42:50 +0100

    Inital project setup

---
+++ app/components/App.js
@@ -0,0 +1,13 @@
+var React = require('react')
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Bede Casino
+      </div>
+    )
+  }
+}
+
+module.exports = App
